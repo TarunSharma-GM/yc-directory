08fae87d3d43e7df1b2eb12014b9cf0b91abc315
TarunSharma-GM <59974666+TarunSharma-GM@users.noreply.github.com>
2025-07-17 19:52:03 +0530

    first commit

---
+++ lib/actions.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { auth } from "@/auth";
+import { parseServerActionsResponse } from "@/lib/utils";
+import { writeClient } from "@/sanity/lib/write-client";
+import slugify from "slugify";
+
+export const creatPitch = async (state:any, form: FormData, pitch: string) => {
+    const session = await auth();
+
+    if(!session) return parseServerActionsResponse({error: "Not Signed in", status:"ERROR"});
+
+    const {title, description, category, link} = Object.fromEntries(
+        Array.from(form).filter(([key]) => key !== "pitch")
+    )
+    const slug = slugify(title as string, {lower:true, strict:true})
+
+    try{
+        const startup = {
+            title,
+            description,
+            category,
+            imgae: link,
+            slug : {
+                _type: slug,
+                current: slug
+            },
+            author: {
+                _type: "reference",
+                _ref: session?.id,
+            },
+            pitch,
+        }
+        const result = await writeClient.create({_type:"startup", ...startup})
+        return parseServerActionsResponse({...result, error:"",status:"SUCCESS"})
+    } catch (error) {
+        console.log(error);
+        return parseServerActionsResponse({error:JSON.stringify(error),status:"ERROR"})
+    }
+}
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c0cbb0302298d19fa05d1de37344173f@o4509678111490048.ingest.us.sentry.io/4509678113783808",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
