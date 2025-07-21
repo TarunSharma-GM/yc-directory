3618fc5c8d3a376270b1eeaa4dcfb2a227db9faf
TarunSharma-GM <59974666+TarunSharma-GM@users.noreply.github.com>
2025-07-21 20:39:28 +0530

    fixing auth issues

--- auth.ts
+++ auth.ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import GitHubProvider from "next-auth/providers/github"
 import { client } from "@/sanity/lib/client"
 import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
 import { writeClient } from "@/sanity/lib/write-client"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
   trustHost: true,
   callbacks: {
     async signIn({user: {name, email, image}, profile:{ id, login, bio}}) {
